c8a1c577a729a724e6e9dc1a2e2fd93b156f0d2f
Aseem Kishore <aseem.kishore@gmail.com>
2012-08-08 17:23:28 -0400

    Switch to compiling LESS on the server-side!
    By dynamically rewriting *.less.css URLs to the lessc.php compiler.

--- _templates/head.php
+++ _templates/head.php
@@ -60,22 +60,7 @@
     <link rel="shortcut icon" href="favicon.ico">
     
     <link rel="stylesheet" href="styles/h5bp-base.css" />
-    <link rel="stylesheet/less" href="styles/common.less.css" />
-    
-    <!-- TEMP for now, downloading raw LESS and converting to CSS in the browser -->
-    <script src="styles/less-1.0.41.min.js"></script>
-    
-    <script>
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-22174557-1']);
-        _gaq.push(['_trackPageview']);
-        
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-    </script>
+    <link rel="stylesheet" href="styles/common.less.css" />
     
     <!-- this is needed before body to enable HTML5 elements in IE8- -->
     <script src="scripts/modernizr-1.7.min.js"></script>
